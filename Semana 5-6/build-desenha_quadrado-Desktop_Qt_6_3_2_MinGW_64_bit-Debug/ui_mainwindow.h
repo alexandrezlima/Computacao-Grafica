fb74a6e2f027415afc986a0ba2323e99e988efa3
Alexandre Lima <alexandrelima888@gmail.com>
2022-11-01 00:53:04 -0300

    fim aula semana 06-07

---
+++ Semana 5-6/build-desenha_quadrado-Desktop_Qt_6_3_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,98 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.3.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+#include "openglwidget.h"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionOpen_File;
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout;
+    OpenGLWidget *openGLWidget;
+    QCheckBox *checkBoxDarkMode;
+    QMenuBar *menubar;
+    QMenu *menuFile;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        actionOpen_File = new QAction(MainWindow);
+        actionOpen_File->setObjectName(QString::fromUtf8("actionOpen_File"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        openGLWidget = new OpenGLWidget(centralwidget);
+        openGLWidget->setObjectName(QString::fromUtf8("openGLWidget"));
+        openGLWidget->setFocusPolicy(Qt::StrongFocus);
+
+        verticalLayout->addWidget(openGLWidget);
+
+        checkBoxDarkMode = new QCheckBox(centralwidget);
+        checkBoxDarkMode->setObjectName(QString::fromUtf8("checkBoxDarkMode"));
+
+        verticalLayout->addWidget(checkBoxDarkMode);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 21));
+        menuFile = new QMenu(menubar);
+        menuFile->setObjectName(QString::fromUtf8("menuFile"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuFile->menuAction());
+        menuFile->addAction(actionOpen_File);
+
+        retranslateUi(MainWindow);
+        QObject::connect(checkBoxDarkMode, SIGNAL(clicked(bool)), openGLWidget, SLOT(toggleDarkMode(bool)));
+        QObject::connect(openGLWidget, SIGNAL(statusBarMessage(QString)), statusbar, SLOT(showMessage(QString)));
+        QObject::connect(actionOpen_File, &QAction::triggered, openGLWidget, qOverload<>(&OpenGLWidget::showFileOpenDialog));
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionOpen_File->setText(QCoreApplication::translate("MainWindow", "Open File", nullptr));
+        checkBoxDarkMode->setText(QCoreApplication::translate("MainWindow", "Dark Mode", nullptr));
+        menuFile->setTitle(QCoreApplication::translate("MainWindow", "File", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
